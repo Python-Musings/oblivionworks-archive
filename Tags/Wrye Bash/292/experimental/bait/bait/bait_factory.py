52f32dd6db2abf3435755d8d391839fa302eb6b0
lojack5 <lojo.jacob@gmail.com>
2011-07-18 23:07:26 +0000

    git-svn-id: https://svn.code.sf.net/p/oblivionworks/code@1400 c80116c5-1073-4541-b2a9-63b9fd661f84

---
+++ Tags/Wrye Bash/292/experimental/bait/bait/bait_factory.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# bait/bait_factory.py
+#
+# GPL License and Copyright Notice ============================================
+#  This file is part of Wrye Bash.
+#
+#  Wrye Bash is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Wrye Bash is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Bash.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  Wrye Bash Copyright (C) 2011 Myk Taylor
+#
+# =============================================================================
+
+import Queue
+import logging
+import multiprocessing
+
+from .view import bait_view
+from .presenter import bait_presenter
+from .model import bait_model
+
+
+_logger = logging.getLogger(__name__)
+
+
+def _create_queue(isMultiprocess):
+    """Creates a communications queue"""
+    if (isMultiprocess is True):
+        return multiprocessing.Queue()
+    else:
+        return Queue.Queue()
+
+def CreateBaitView(parent, presenter=None, model=None, isMultiprocess=False):
+    _logger.debug("creating BAIT components")
+
+    if not model is None:
+        _logger.debug("using custom model (class: %s)", model.__class__)
+    else:
+        _logger.debug("instantiating default model")
+        model = bait_model.BaitModel()
+
+    if not presenter is None:
+        _logger.debug("using custom presenter (class: %s)", presenter.__class__)
+    else:
+        _logger.debug("instantiating default presenter")
+        presenter = bait_presenter.BaitPresenter(model, _create_queue(isMultiprocess))
+    return bait_view.BaitView(parent, presenter)
---
+++ Tags/Wrye Bash/292/experimental/bait/bait/model/__init__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+# bait/model/__init__.py
+#
+# GPL License and Copyright Notice ============================================
+#  This file is part of Wrye Bash.
+#
+#  Wrye Bash is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Wrye Bash is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Bash.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  Wrye Bash Copyright (C) 2011 Myk Taylor
+#
+# =============================================================================
+
+from . import impl
+
+
+ROOT_NODE_ID = impl.nodeCounter.next()
+
+NODE_TYPE_ROOT = 0x01
+NODE_TYPE_PACKAGE = 0x02
+NODE_TYPE_GROUP = 0x04
+NODE_TYPE_SUBPACKAGE = 0x08
+NODE_TYPE_DIRECTORY = 0x10
+NODE_TYPE_FILE = 0x20
+
+ERROR_PERMISSIONS_READ = 1
+ERROR_PERMISSIONS_WRITE = 2
+ERROR_DISK_FULL = 3
+# TODO: ...
+
+UPDATE_TYPE_ATTRIBUTES = 0x01
+UPDATE_TYPE_CHILDREN = 0x02
+UPDATE_TYPE_DETAILS = 0x04
+UPDATE_TYPE_ERROR = 0x08
+
+# for node updates, the tuple is: (updateType, nodeType, nodeId, version)
+UPDATE_TUPLE_IDX_TYPE = 0
+UPDATE_NODE_TUPLE_IDX_NODE_TYPE = 1
+UPDATE_NODE_TUPLE_IDX_NODE_ID = 2
+UPDATE_NODE_TUPLE_IDX_VERSION = 3
+
+# for error, the tuple is: (UPDATE_TYPE_ERROR, errorCode, resourceName)
+UPDATE_ERROR_TUPLE_IDX_CODE = 1
+UPDATE_ERROR_TUPLE_IDX_RESOURCE_NAME = 2
+
+class _VersionedData:
+    """version gets incremented for every change to the data.  clients can check the
+    version to ensure an update is for data newer than what it already has"""
+    def __init__(self):
+        self.version = 0
