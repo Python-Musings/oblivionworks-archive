8e31ae33a5e8b93e97d68230faa9df1c87f8317a
lojack5 <lojo.jacob@gmail.com>
2012-12-25 02:51:10 +0000

    - add checks for dependencies on startup - debug.bat file works like trunk now
    git-svn-id: https://svn.code.sf.net/p/oblivionworks/code@2879 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Programs/Wrye Bash Python 3/Mopy/src/bash.py
+++ Programs/Wrye Bash Python 3/Mopy/src/bash.py
@@ -26,11 +26,78 @@
 # Imports ----------------------------------------------------------------------
 #--Standard
 import os
+import sys
+
+def VerifyRequirements():
+    """Check to see if all required dependancies are installed."""
+    errors = []
+    #--Python 3.2
+    if sys.version_info[0:2] != (3,2):
+        errors.append(_('Python 3.2 is required.  Current version %(version)s')
+                      % {'version':sys.version_info})
+    #--wxPython
+    try:
+        import wx
+        haveWx = True
+    except ImportError:
+        errors.append(_('wxPython is required.  Get wxPhoenix: %(url)s')
+                      % {'url':'http://wiki.wxpython.org/ProjectPhoenix'})
+        haveWx = False
+    #--pywin32
+    try:
+        import win32api
+        version = win32api.GetFileVersionInfo(win32api.__file__,'\\')
+        version = version['FileVersionLS'] >> 16
+        if version < 218:
+            errors.append(
+                _('pywin32 218 is required.  Current version %(version)s')
+                % {'version':version})
+    except ImportError:
+        errors.append(_('pywin32 218 is required.'))
+
+    if errors:
+        # First, try to show the error in the GUI
+        msg = '\n'.join(errors)
+        shown = False
+        if haveWx:
+            try:
+                from bash.balt import ShowError
+                ShowError(None,msg)
+                shown = True
+            except:
+                pass
+        if not shown:
+            # wx failed, try Tkinter
+            try:
+                import Tkinter
+                root = Tkinter.Tk()
+                frame = Tkinter.Frame(root)
+                frame.pack()
+                button = Tkinter.Button(frame,text=_('Quit'),fg='red',
+                                        command=root.destroy,pady=15,borderwidth=15,
+                                        relief=Tkinter.GROOVE)
+                button.pack(fill=Tkinter.BOTH,expand=1,side=Tkinter.BOTTOM)
+                text = Tkinter.Text(frame)
+                text.insert(Tkinter.END,msg)
+                text.config(state=Tkinter.DISABLED)
+                text.pack()
+                root.mainloop()
+                shown = True
+            except:
+                pass
+        if not shown:
+            # even Tkinter failed
+            print(msg)
+        return False
+    return True
 
 def main():
     #--Setup translations
     from src.bolt import Translations
     Translations.Install()
+    #--Check for dependencies
+    if not VerifyRequirements():
+        return
     #--Start wxApp
     import wx
     app = wx.App()
