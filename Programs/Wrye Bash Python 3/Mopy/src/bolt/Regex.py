65a1e3822862d4ffea0ffbb02830df66adac7434
lojack5 <lojo.jacob@gmail.com>
2012-12-27 04:19:20 +0000

    add license to Regex.py
    git-svn-id: https://svn.code.sf.net/p/oblivionworks/code@2900 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Programs/Wrye Bash Python 3/Mopy/src/bolt/Regex.py
+++ Programs/Wrye Bash Python 3/Mopy/src/bolt/Regex.py
@@ -1,5 +1,37 @@
+# -*- coding: utf-8 -*-
+# src/bolt/Regex.py
+#
+# GPL License and Copyright Notice =============================================
+#  This file is part of Wrye Bash.
+#
+#  Wrye Bash is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  Wrye Bash is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Bash; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  Wrye Bash copyright (C) 2005, 2006, 2007, 2008, 2009 Wrye
+#
+# ==============================================================================
+
+"""This module holds useful regex's that can be used."""
+
+# Imports ----------------------------------------------------------------------
 import re
+
+# Regex's ----------------------------------------------------------------------
 
 # Regex to find URLs in blocks of text
 reURL = re.compile(r'\b(?:(?:https?|ftp|file)://|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])',re.I)
+
+# Regex to find URLs in blocks of text, but in a wiki/other markup type fasion:
+#  [[title|url]]
 reURLCode = re.compile(r'\[\[(.+?)\|((?:(?:https?|ftp|file)://|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$]))\]\]',re.I)
