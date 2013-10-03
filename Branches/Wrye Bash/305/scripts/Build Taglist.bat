8365ba684db01884f5ff5b941bd488cc0e119835
Sharlikran <danielhmpdx16@gmail.com>
2013-10-03 15:59:47 +0000

    [WryeBash] Updating dev-sharlikran branch - Creating a 305 Branch with changes from previous commits by tox2ik -- For updating skyrim.py I will work with the same code in trunk -- This is created for reference to preserve tox2ik's submissions
    git-svn-id: https://svn.code.sf.net/p/oblivionworks/code@3077 c80116c5-1073-4541-b2a9-63b9fd661f84

---
+++ Branches/Wrye Bash/305/scripts/Build Taglist.bat
@@ -0,0 +1,12 @@
+@echo off
+title Wrye Bash Taglist Generator
+echo.
+echo.
+
+"C:\Python27\python.exe" "E:\oblivionworks_rl\Programs\Wrye Bash\scripts\mktaglist.py"
+
+echo.
+echo.
+
+echo. All Done!
+pause
